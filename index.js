ec242e5ba9405503bc0092f506f46d5aa3a2fc25
Robert Wise <robcodehub@gmail.com>
2019-12-17 14:49:16 +1100

    started nav test

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-import { AppRegistry } from 'react-native';
+import { Navigation } from "react-native-navigation";
+
+import { AppRegistry } from 'react-native'; //DELETED IN DEMO
 import App from './App.js';
 
-AppRegistry.registerComponent('ViroSample', () => App);
+//AppRegistry.registerComponent('ViroSample', () => App);
+
+Navigation.registerComponent(`navigation.playground.WelcomeScreen`, () => App);
+
+
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      component: {
+        name: "navigation.playground.WelcomeScreen"
+        }
+     }
+    });
+  });
+
+
+
 
 // The below line is necessary for use with the TestBed App
 AppRegistry.registerComponent('ViroSample', () => App);
+
+
